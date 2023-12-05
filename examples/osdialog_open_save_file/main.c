a653930411cdbece00e441c226db5f7679f0c52c
Tré Dudman <19359682+Tremus@users.noreply.github.com>
2023-12-05 10:37:45 +1000

    Added osdialog example for Windows

---
+++ examples/osdialog_open_save_file/main.c
@@ -0,0 +1,29 @@
+#ifdef _WIN32
+#define _CRT_SECURE_NO_WARNINGS 1
+#define snwprintf _snwprintf
+#include <osdialog_win.c>
+#endif
+
+#include <osdialog.c>
+
+int main()
+{
+    char* filename = osdialog_file(OSDIALOG_OPEN, NULL, NULL, NULL);
+    if (filename != NULL)
+    {
+        printf("You selected the file %s with Open dialog\n", filename);
+        OSDIALOG_FREE(filename);
+    }
+    else
+        printf("Failed Open dialog...\n");
+
+    filename = osdialog_file(OSDIALOG_SAVE, NULL, NULL, NULL);
+    if (filename != NULL)
+    {
+        printf("You selected the file %s with Save dialog\n", filename);
+        OSDIALOG_FREE(filename);
+    }
+    else
+        printf("Failed Save dialog...\n");
+    return filename == NULL;
+}
